3f3dcec37a93ad7ba99e4583d47389e1b5dfb2e9
Om Patel <opatel1370@gmail.com>
2026-01-21 16:39:08 -0700

    Completed lab exercise

---
+++ code/app/src/main/java/com/example/listycity/CityArrayAdapter.java
@@ -0,0 +1,44 @@
+package com.example.listycity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class CityArrayAdapter extends ArrayAdapter<City> {
+    public CityArrayAdapter(Context context, ArrayList<City> cities) {
+        super(context, 0, cities);
+    }
+
+    // Called when we want to generate an element of the array
+    // position: position of the element in the array
+    // convertView: use view that already exists
+    // parents:
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view;
+        // Inflate pre-existing view instead of creating new one?
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.content, parent, false);
+        } else {
+            view = convertView;
+        }
+
+        City city = getItem(position);
+        TextView cityName = view.findViewById(R.id.city_text);
+        TextView provinceName = view.findViewById(R.id.province_text);
+
+        cityName.setText(city.getName());
+        provinceName.setText(city.getProvince());
+
+        return view;
+    }
+}
